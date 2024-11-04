4e6c00de1fe2c702a74504938f20ab9dc9d1854a
cgcl_unravel <vastrockhuang162@gmail.com>
2024-11-04 09:18:40 +0000

    v0.0.3: support example: pangolin converter

---
+++ examples/pangolin/pangolin_convert.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <fstream>
+#include <filesystem>
+
+#include "CLI11/CLI11.hpp"
+
+#include "pangolin_converter.hpp"
+
+using namespace graph_loader;
+namespace fs = std::filesystem;
+
+using vidType = int32_t;
+using eidType = int64_t;
+
+int main(int argc, char** argv) {
+    CLI::App app;
+
+    LoaderOpts opts = LoaderOpts::WithHeader().set_is_directed(false);
+    std::string filepath;
+    std::string output_dir;
+
+    app.add_option("-i, --input_graph", filepath, "input graph dataset file")->required();
+    app.add_option("-o,--output_dir", output_dir, "output directory to save result");
+    app.add_flag("-r,--reorder", opts.do_reorder, "reorder vertex id in graph dataset to compress (defalut 'false')");
+    app.add_flag("--rm_self", opts.rm_self_loop, "remove self loop edges in graph (defalut 'false')");
+    CLI11_PARSE(app, argc, argv);
+
+    std::cout << "[WARN] input graph file must be a undirected graph!\n";
+
+    if (output_dir.empty()) {
+        output_dir = fs::path(filepath).parent_path().string() + "/";
+    } else if (output_dir.back() != '/') {
+        output_dir += "/";
+    }
+
+    {
+        if (!fs::exists(output_dir)) {
+            if (!fs::create_directories(output_dir)) {
+                std::cout << "[ERROR] output directory is not exist but create failed\n";
+                return -1;
+            }
+        } else if (!fs::is_directory(output_dir)) {
+            std::cout << "[ERROR] output path is not a directory\n";
+            return -1;
+        }
+    }
+    
+    PangolinMeta<vidType> meta = 
+        PangolinConverter<vidType, eidType>::Load(filepath, opts);
+    
+    auto& adj_list = meta.adj_list;
+    vidType n = meta.adj_list.size();
+
+    std::vector<eidType> row_offsets(n + 1);
+    std::vector<vidType> column_indices;
+
+    row_offsets[0] = 0;
+    for (int i = 0; i < n; ++i) {
+        std::sort(adj_list[i].begin(), adj_list[i].end());
+        for (const auto& neighbor : adj_list[i]) {
+            column_indices.push_back(neighbor);
+        }
+        row_offsets[i + 1] = column_indices.size();
+    }
+
+    eidType m = column_indices.size();
+
+    {
+        std::ofstream meta_file(output_dir + "graph.meta.txt");
+        meta_file << n << std::endl;
+        meta_file << m << std::endl;
+        meta_file << sizeof(vidType) << ' ' << sizeof(eidType) << ' ' << 1 << ' ' << 2 << std::endl;
+        meta_file << meta.max_degree << std::endl;
+        meta_file << 0 << std::endl << 1 << std::endl << 1 << std::endl;
+    }
+ 
+    {
+        std::ofstream vertex_file(output_dir + "graph.vertex.bin", std::ios::binary);
+        vertex_file.write(reinterpret_cast<const char*>(row_offsets.data()), (n + 1) * sizeof(eidType));
+    }
+
+    {
+        std::ofstream edge_file(output_dir + "graph.edge.bin", std::ios::binary);
+        edge_file.write(reinterpret_cast<const char*>(column_indices.data()), m * sizeof(vidType));
+    }
+
+    std::cout << "CONVERT: " << filepath << " OK\n";
+
+    return 0;
+}
---
+++ examples/pangolin/pangolin_converter.hpp
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include <cassert>
+
+#include "graph_loader/loader.hpp"
+
+template <typename vertex_t>
+struct PangolinMeta {
+    std::vector<std::vector<vertex_t>> adj_list;
+    vertex_t max_degree = 0;
+};
+
+template <typename vertex_t, typename edge_t>
+class PangolinConverter {
+public:
+    using Loader = graph_loader::CoreLoader<vertex_t, edge_t, graph_loader::empty_t>;
+
+    static PangolinMeta<vertex_t> 
+    Load(const std::string& filepath, graph_loader::LoaderOpts opts) {
+        assert(!opts.is_directed);
+
+        PangolinMeta<vertex_t> meta;
+
+        auto pre_load_func = [&](vertex_t num_v, edge_t num_e) {
+            meta.adj_list.resize(num_v);
+        };
+
+        auto edge_load_func = [&](edge_t eidx, vertex_t& src, vertex_t& dst) -> bool {
+            meta.adj_list[src].push_back(dst);
+            if (src != dst) {
+                meta.adj_list[dst].push_back(src);
+            }
+            meta.max_degree = std::max<vertex_t>(meta.max_degree, meta.adj_list[src].size());
+            meta.max_degree = std::max<vertex_t>(meta.max_degree, meta.adj_list[dst].size());
+            return true;
+        };
+
+        Loader::Load(filepath, opts, edge_load_func, pre_load_func);
+
+        return meta;
+    }
+     
+};
